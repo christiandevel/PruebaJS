e6531abbbb89c68172d669368189e722df336875
christian moreno <christianmorenoti@gmail.com>
2021-11-30 16:09:28 -0500

    Preguntas condicionales

---
+++ condicionales.js
@@ -0,0 +1,18 @@
+// 1. ¿Que es una condicional?
+/**
+ * Intruccion o grupo de instracciones que se pueden
+ * ejecutar o no en funcion del valor de una condicion
+ */
+
+// 2. ¿Que tipos de condicones existen en JS y cuales son sus diferencias
+/**
+ * En JS existe el condicional (if-else) ternario (: ?) y el switch
+ * La principal difencia radica en los elementos de las condicional
+ * en el primero solo tenemos dos operaciones, en el segundo tres y el ultimo 
+ * opciones finitas de acuerdo a la condicional
+ */
+
+// 3. ¿Puedo combinar funciones y condicionales?
+/**
+ * Si se puede
+ */
