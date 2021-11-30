39a1bd949958aa74da4f66e140c5c9e5a4d44ca3
christian moreno <christianmorenoti@gmail.com>
2021-11-30 15:34:58 -0500

    Preguntas de funciones

---
+++ funciones.js
@@ -0,0 +1,18 @@
+// 1. ¿Que es una funcion ?
+/**
+ *Es un bloque de codigo que ejecuta un procedimiento, 
+ *contiene en algunos caso una entrada y genera una salida 
+ */
+
+// 2. ¿Cuando me sirve usar una funcion en mi codigo?
+/**
+ *	Cuando se repiten varias lineas de codigo 
+ */
+
+//  3. ¿Cual es la diferencia entre parametros y argumentos de una funcion?
+/**
+ * Parametros son los que definimos en una funcion
+ * Tenemos la funcion suma la cual recibe los parametro num1 y num2
+ * Argumentos son los que le pasamos a la funcion
+ * LLamamos la funcion suma a la cual le pasamos los argumentos 1 y 2
+ */
