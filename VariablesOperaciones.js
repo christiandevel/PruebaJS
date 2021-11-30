b3d57b10c11fe06ec24b2b0a4b1382687c55b3d8
christian moreno <christianmorenoti@gmail.com>
2021-11-30 12:52:55 -0500

    Preguntas de Variables y operaciones

---
+++ VariablesOperaciones.js
@@ -0,0 +1,23 @@
+// ******* 1. ¿Que es una variables y para que sirve? *******
+
+/*
+Una variables es una espacio en memoria que reservamos para guardar un valor.
+*/
+
+// ******* 2. ¿Cual es la diferencia entre declarar e inicializar una variables? *******
+
+/*
+Cuando declaramos una variable reservamos su espacio en memoria y
+cuando la inicializamos asignamos un valor a ese espacion que reservamos
+*/
+
+// ******* 3. ¿Cual es la diferencia entre sumar numeros y concatenar strings? *******
+/*
+Sumar numeros hace refencia a la operacion aritmetica y concatenar SVGStrings 
+hace referencia a la union de dos o varias variables de tipo string
+*/
+
+// ******* 4. ¿Cual operador me permite sumar y concatenar? *******
+/*
+El caracter '+' 
+*/
