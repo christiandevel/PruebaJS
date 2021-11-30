eccf6341221332690df58868b09a80a6b8f7ae74
christian moreno <christianmorenoti@gmail.com>
2021-11-30 12:59:20 -0500

    Preguntas de Datos y tipo de datos

---
+++ NombreTipoDato.js
@@ -0,0 +1,13 @@
+let nombre = 'Christian';
+let apellido = 'Moreno';
+let nombreUser = 'Christian_Andres';
+let edad = 22;
+let correo = 'christianmorenoti@gmail.com';
+let mayorEdad = true;
+let DineroAhorrado = -1000;
+let deudas = 5000;
+
+
+let nombreCompleto = `${nombre} ${apellido}`;
+let dineroReal = DineroAhorrado - deudas;
+console.log(dineroReal)
